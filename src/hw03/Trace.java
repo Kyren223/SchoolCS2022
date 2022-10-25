cda392c1b04d85c067690b19fb29a981e730f004
BuildTools <unconfigured@null.spigotmc.org>
2022-10-25 21:50:33 +0300

    Added hw03 and class Trace Tracing 2nd way to find the maximum number in a given array

---
+++ src/hw03/Trace.java
@@ -0,0 +1,70 @@
+package hw03;
+
+public class Trace {
+
+    public static void main(String[] args) {
+
+        /*
+
+        arr = {4, 6, 9, 7, 1, 5, 8}
+        begin = 0 end = 6
+
+        max(X1, X2)
+        middle = 3
+
+        begin = 0 end = 3
+        X1 = max(Y1, Y2)
+        middle = 1
+
+        begin = 0 end = 1
+        Y1 = max(Z1, Z2)
+        middle = 0
+
+        begin = 0 end = 0
+        Z1 = arr[0] = 4
+
+        begin = 1 end 1
+        Z1 = arr[1] = 6
+
+        Y1 = max(4, 6) 6
+
+        begin = 2 end = 3
+        Y2 = max(Z3, Z4)
+
+        begin = 2 end = 2
+        Z3 = arr[2] = 9
+
+        begin = 3 end = 3
+        Z4 = arr[3] = 7
+
+        Y2 = max(9, 7) = 9
+
+        X1 = max(6, 9) = 9
+
+        begin = 4 end = 6
+        X2 = (Y3, Y4)
+        middle = 5
+
+        begin = 4 end = 5
+        Y3 = max(Z5, Z6)
+        middle = 4
+
+        begin = 4 end = 4
+        Z5 = arr[4] = 1
+
+        begin = 5 end = 5
+        Z6 = arr[5] = 5
+
+        Y3 = max(1, 5) = 5
+
+        begin = 6 end = 6
+        Y4 = arr[6] = 8
+
+        X2 = max(5, 8) = 8
+
+        RESULT = max(9, 8) = 9
+
+         */
+
+    }
+}
