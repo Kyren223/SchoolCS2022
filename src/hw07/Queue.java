d09a53125440ada12febbc51ea34d9ef10a29320
Liad Twito <ulmliad223@gmail.com>
2022-12-30 12:42:12 +0200

    Added hw07 - Queue class

---
+++ src/hw07/Queue.java
@@ -0,0 +1,50 @@
+package hw07;
+
+import hw05.Node;
+
+public class Queue<T> {
+
+    private Node<T> first;
+    private Node<T> last;
+    private int size; // Just for convenience
+    
+    public Queue() {
+        size = 0;
+        first = null;
+        last = null;
+    }
+    
+    boolean isEmpty() {
+        return size == 0;
+    }
+    
+    void insert(T x) {
+        Node<T> insertionNode = new Node<>(x);
+        size++;
+        if (last == null) {
+            last = insertionNode;
+            first = insertionNode;
+            return;
+        }
+        last.setNext(insertionNode);
+        last = last.getNext();
+    }
+    
+    T remove() {
+        if (isEmpty()) return null;
+        Node<T> deletionNode = first;
+        first = first.getNext();
+        size--;
+        return deletionNode.getValue();
+    }
+    
+    T head() {
+        if (isEmpty()) return null;
+        return first.getValue();
+    }
+    
+    @Override
+    public String toString() {
+        return first.toString();
+    }
+}
